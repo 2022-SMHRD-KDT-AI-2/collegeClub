fd1b17eb8e0eee599f115b7d2f51178b4954a4a9
강건우 <98583833+KangGunWoo@users.noreply.github.com>
2022-05-18 14:19:02 +0900

    220518/0218/강건우

--- react-flask-kimd0/react-app/src/components/Main.js
+++ react-flask-kimd0/react-app/src/components/Main.js
@@ -1,98 +1,75 @@
-import React from 'react'; 
-import { Link } from 'react-router-dom'; 
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Main.css';
+import axios from 'axios';
+import logo from '../img/logo.png';
+import bar01 from '../img/meneBar_01.png';
+import bar02 from '../img/menuBar_02.png';
+import bar03 from '../img/menuBar_03.png';
+import bar04 from '../img/menuBar_04.png';
+import bar05 from '../img/menuBar_05.png';
+
+
 
 //시나리오 선택 화면
+function link() {
+  setTimeout(function () {
+    <Link to="/Screen1">213</Link>
+  }, 5);
+}
 
-function Scene() {
-    return <div className="wrap">
-      <div className="header">
-        <div className="campus"><div className='incam'></div></div>
-      </div>
-      <div className="sec1"></div>
-      <div className="sec2"></div>
-      <div className="footer">
-        <div className="levelsec">
-          <div className="pre"><div className="preText">이전</div></div>
-          <div className="level">
-            <p className="que">Q3</p>
-            <p className="que">○ ● ○ ○ ○ ○ ○ ○</p>
-          </div>
-          <div className="next"><div className="nextText">다음</div></div>
-        </div>
-        <div className="footerImg"></div>
-      </div>
-    </div >
-  }
-  
-  function Quiz() {
-    return <div className="wrap">
-      <div className="header">
-        <div className="campus"><div className='incam'></div></div>
-      </div>
-      <div className="sec1"></div>
-      <div className="sec2">
-        <div className="quizButton">
-          <div className="quizLeft"></div>
-          <div className="quizBar"></div>
-          <div className="quizRight"></div>
-        </div>
-      </div>
-      <div className="footer">
-        <div className="levelsec">
-          <div className="pre"><div className="preText">이전</div></div>
-          <div className="level">
-            
-          </div>
-          <div className="next"><div className="nextText">다음</div></div>
-        </div>
-        <div className="footerImg"></div>
-      </div>
-    </div >
-  }
-  
-  function Quiz2() {
-    return <div className="wrap">
-      <div className="header">
-        <div className="campus"><div className='incam'></div></div>
-      </div>
-      <div className="sec1"></div>
-      <div className="sec2">
-        <div className="quizButton">
-          <div className="quizLeft"></div>
-          <div className="quizCenter"></div>
-          <div className="quizRight"></div>
-        </div>
-      </div>
-      <div className="footer">
-        <div className="levelsec">
-          <div className="pre"><div className="preText">이전</div></div>
-          <div className="level">
-            
-          </div>
-          <div className="next"><div className="nextText">다음</div></div>
-        </div>
-        <div className="footerImg"></div>
-      </div>
-    </div >
-  }
+const Main = (props) => {
 
-const Main = (props) => { 
-    return ( 
-        <> 
-            <h3>안녕하세요. 메인페이지 입니다.</h3> 
-            <ul> 
-                <Link to="/screen1">
-                    <li>1번상품</li>
-                </Link> 
-                <Link to="/product/2">
-                    <li>2번상품</li>
-                </Link> 
-                <Link to="/result">
-                    <li>result</li>
-                </Link>
-            </ul> 
-        </> 
-    ); 
-}; 
+
+
+  return (
+    <>
+      <div></div>
+      <div id="main">
+        <img src={logo} width="200px" />
+
+        <div className="sce">
+          <div className="sce1">
+
+            <div className="sce11">
+              <button id='sc1' type='button' onClick={link}>시나리오 1</button>
+            </div>
+          </div>
+          <div className="sce2">
+            <div className="sce22">
+              <button id='sc2' type='button'>시나리오 2</button>
+            </div>
+          </div>
+          <div className="sce3">
+            <div className="sce33">
+              <button id='sc3' type='button'>시나리오 3</button>
+            </div>
+          </div>
+        </div>
+        <div id="menuBar">
+          <div>
+            <img src={bar01} id="menuBarIcon1" />
+          </div>
+          <div>
+            <img src={bar02} id="menuBarIcon2" />
+          </div>
+          <div>
+            <img src={bar03} id="menuBarIcon3" />
+          </div>
+          <div>
+            <img src={bar04} id="menuBarIcon4" />
+          </div>
+          <div>
+            <img src={bar05} id="menuBarIcon5" />
+          </div>
+        </div>
+      </div>
+
+
+
+
+    </>
+  );
+};
 export default Main;
 
