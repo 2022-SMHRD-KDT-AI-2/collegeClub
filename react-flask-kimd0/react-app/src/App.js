c02aee129766c56ffe56893eb572cd2e756f7b75
양세영 <99248841+se0ce4@users.noreply.github.com>
2022-05-18 11:32:46 +0900

    05181132

--- react-flask-kimd0/react-app/src/App.js
+++ react-flask-kimd0/react-app/src/App.js
@@ -48,10 +48,36 @@ function Quiz() {
   </div >
 }
 
+function Quiz2() {
+  return <div className="wrap">
+    <div className="header">
+      <div className="campus"><div className='incam'></div></div>
+    </div>
+    <div className="sec1"></div>
+    <div className="sec2">
+      <div className="quizButton">
+        <div className="quizLeft"></div>
+        <div className="quizCenter"></div>
+        <div className="quizRight"></div>
+      </div>
+    </div>
+    <div className="footer">
+      <div className="levelsec">
+        <div className="pre"><div className="preText">이전</div></div>
+        <div className="level">
+          
+        </div>
+        <div className="next"><div className="nextText">다음</div></div>
+      </div>
+      <div className="footerImg"></div>
+    </div>
+  </div >
+}
+
 function App() {
   return (
     <div>
-      <Quiz></Quiz>
+      <Quiz2></Quiz2>
     </div>
   );
 }
