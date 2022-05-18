3fec0a8690f3d5bf6bea7a720398488acd4f0253
양세영 <99248841+se0ce4@users.noreply.github.com>
2022-05-18 15:08:26 +0900

    1

--- react-flask-kimd0/react-app/src/App.js
+++ react-flask-kimd0/react-app/src/App.js
@@ -2,8 +2,12 @@ import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import Main from './components/Main';
+<<<<<<< Updated upstream
 import Screen1 from './components/Screen1';
 import Result from './components/result'
+=======
+import Quiz2 from './components/Quiz2';
+>>>>>>> Stashed changes
 import axios from 'axios';
 
 import logo from './logo.svg';
@@ -16,8 +20,12 @@ function App() {
       <BrowserRouter> 
         <Routes> 
           <Route path="/" element={<Main />}></Route> 
+<<<<<<< Updated upstream
           <Route path="/screen1/" element={<Screen1 />}></Route> 
           <Route path="/result/" element={<Result />}></Route> 
+=======
+          <Route path="/quiz2*" element={<Quiz2 />}></Route> 
+>>>>>>> Stashed changes
           {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */} 
         </Routes> 
       </BrowserRouter>
