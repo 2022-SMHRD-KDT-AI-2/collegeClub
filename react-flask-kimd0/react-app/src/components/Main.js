f63a69177eac72a1fbcdd50dfbb3ce031c1b494a
강건우 <98583833+KangGunWoo@users.noreply.github.com>
2022-05-18 15:03:34 +0900

    220518/0303/kang

--- react-flask-kimd0/react-app/src/components/Main.js
+++ react-flask-kimd0/react-app/src/components/Main.js
@@ -11,12 +11,6 @@ import bar05 from '../img/menuBar_05.png';
 
 
 
-//시나리오 선택 화면
-function link() {
-  setTimeout(function () {
-    <Link to="/Screen1">213</Link>
-  }, 5);
-}
 
 const Main = (props) => {
 
@@ -26,42 +20,29 @@ const Main = (props) => {
     <>
       <div></div>
       <div id="main">
-        <img src={logo} width="200px" />
+        <img className='logo' src={logo} width="200px" />
 
         <div className="sce">
           <div className="sce1">
 
             <div className="sce11">
-              <button id='sc1' type='button' onClick={link}>시나리오 1</button>
+              <button id='sc1' type='button'> <Link to="/Screen1">  <p className='start'>시나리오 1</p></Link></button>
+
             </div>
           </div>
           <div className="sce2">
             <div className="sce22">
-              <button id='sc2' type='button'>시나리오 2</button>
+              <button id='sc2' type='button'>  <Link to="/Screen1"><p className='start'>시나리오 2</p></Link></button>
             </div>
           </div>
           <div className="sce3">
             <div className="sce33">
-              <button id='sc3' type='button'>시나리오 3</button>
+              <button id='sc3' type='button'> <Link to="/Screen1"> <p className='start'>시나리오 3</p></Link></button>
             </div>
           </div>
         </div>
         <div id="menuBar">
-          <div>
-            <img src={bar01} id="menuBarIcon1" />
-          </div>
-          <div>
-            <img src={bar02} id="menuBarIcon2" />
-          </div>
-          <div>
-            <img src={bar03} id="menuBarIcon3" />
-          </div>
-          <div>
-            <img src={bar04} id="menuBarIcon4" />
-          </div>
-          <div>
-            <img src={bar05} id="menuBarIcon5" />
-          </div>
+
         </div>
       </div>
 
