aad396175e974182e1be9bbb273691b7666ad252
양세영 <99248841+se0ce4@users.noreply.github.com>
2022-05-19 09:39:15 +0900

    220519/0939/ㅛㅁ

--- react-flask-kimd0/react-app/src/App.js
+++ react-flask-kimd0/react-app/src/App.js
@@ -6,6 +6,7 @@ import Main from './components/Main';
 import Screen1 from './components/Screen1';
 import Result from './components/result'
 
+import Quiz1 from './components/Quiz1';
 import Quiz2 from './components/Quiz2';
 
 import axios from 'axios';
@@ -24,6 +25,7 @@ function App() {
           <Route path="/screen1/" element={<Screen1 />}></Route> 
           <Route path="/result/" element={<Result />}></Route> 
 
+          <Route path="/quiz1*" element={<Quiz1 />}></Route> 
           <Route path="/quiz2*" element={<Quiz2 />}></Route> 
 
           {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */} 
--- react-flask-kimd0/react-app/src/components/Quiz1.js
+++ react-flask-kimd0/react-app/src/components/Quiz1.js
@@ -1,10 +1,9 @@
 import React from 'react'; 
+// import { Link } from 'react-router-dom';
+import './Quiz1.css';
 
-import './App.css';
-import Quiz2 from './components/Quiz2';
 
-
-//시나리오 선택 화면
+//퀴즈 2버튼 화면
 
 const Quiz1 = (props) => { 
     return ( 
@@ -16,16 +15,14 @@ const Quiz1 = (props) => {
       <div className="sec1"></div>
       <div className="sec2">
         <div className="quizButton">
-          <button className="quiz1Left"></button>
-          <button className="quiz1Center"></button>
-          <button className="quiz1Right"></button>
+          <button className="quiz1Left">A</button>
+          <button className="quiz1Right">B</button>
         </div>
       </div>
       <div className="footer">
         <div className="levelsec">
           <div className="pre"><div className="preText">이전</div></div>
           <div className="level">
-            
           </div>
           <div className="next"><div className="nextText">다음</div></div>
         </div>
@@ -35,5 +32,5 @@ const Quiz1 = (props) => {
         </> 
     ); 
 }; 
-export default Quiz2;
+export default Quiz1;
 
