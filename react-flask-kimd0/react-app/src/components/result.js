18078655290255dc1bca3127db9e053cc913746d
강건우 <98583833+KangGunWoo@users.noreply.github.com>
2022-05-20 10:09:20 +0900

    123

--- react-flask-kimd0/react-app/src/components/result.js
+++ react-flask-kimd0/react-app/src/components/result.js
@@ -30,52 +30,55 @@ function useResult() {
             {loading ? (
                 'loading...'
             ) : (
-                <div>
-                    <h3>테스트 결과</h3>
-                    <h5>당신은 00 유형입니다.</h5>
-                    <div className='res'>
-                        <div className='res1'>
-                            <div className='stat1'>
-                                {data[1]}
-                                {data[2]}
-                                {data[3]}
 
-                                <div className='hexagon'>
-                                </div>
-                                {data[4]}
-                                {data[5]}
-                                {data[6]}
-                            </div>
-                            <div className='result1'>1</div>
-                        </div>
-                        <div className='res2'>
-                            <div className='stat2'>
-                                {data[1]}
-                                {data[2]}
-                                {data[3]}
 
-                                <div className='hexagon'>
-                                </div>
-                                {data[4]}
-                                {data[5]}
-                                {data[6]}
-                            </div>
-                            <div className='result2'></div>
-                        </div>
+
+
+
+
+
+
+
+                <div className='wrap'>
+                    <div className='Header'>
+                        <div className='title'>테스트 결과</div>
+                        <div className='TypeResult'>당신은 {data} 유형입니다</div>
                     </div>
 
-                    <div className='resultText'></div>
+
+                    <div className='section'>
+                        <div className='Hexagon1'></div>
+                        <div className='point1'></div>
+                        <div className='point2'></div>
+                        <div className='Hexagon2'></div>
 
 
-                    <div className='menubar'></div>
 
-                    <div className='btn'>
-                        <button type='button' className='btn1'>button1</button>
-                        <button type='button' className='btn2'>button2</button>
+
+
+
+
+
+
+
                     </div>
+
+
+
+
+
 
 
                 </div>
+
+
+
+
+
+
+
+
+
             )
             }
 
