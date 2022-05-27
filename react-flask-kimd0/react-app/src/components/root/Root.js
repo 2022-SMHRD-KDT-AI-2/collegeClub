64144994d943e13705e9eb2e91c83087f1cb0823
강건우 <98583833+KangGunWoo@users.noreply.github.com>
2022-05-27 10:23:02 +0900

    2022/05/27 10:22

--- react-flask-kimd0/react-app/src/components/root/Root.js
+++ react-flask-kimd0/react-app/src/components/root/Root.js
@@ -34,23 +34,26 @@ function Root() {
     for (let i = 0; i < data.length; i++) {
       result.push(<div className='Buttondiv' id={"Buttondiv" + i}><button className='Buttonset' id={"Buttonset" + i} key={i}>{data[i].club_name + "  "}</button></div>);
     }
+    result.push(<div className='Buttondiv' id="Buttondiv7"><button className='Buttonset' id="Buttonset7">ㅎㅇㅇ</button></div>)
     return result;
   };
+
 
 
   return (
     <>
       <Header></Header>
-      <div className='clublist'>
+      <div className='Clublistgo'>★동아리 리스트 바로가기</div>
 
-        <div className='render'>{rendering()}</div>
-      </div>
+
+      <div className='render'>{rendering()}</div>
+
       <div className='Checkdiv'>
         <div className='Checkinner'>
-          <Link to="/main"><button className='Check' type='button'>성향검사하기</button></Link>
+          <Link to="/main"><button className='Check1' id='Checkid1' type='button'>성향검사하기</button><button className='Check1' id='Checkid2' type='button'>></button></Link>
         </div>
         <div className='Checkinner2'>
-          <Link to="/result"><button className='Check' type='button'>자신의 성향 보기</button></Link>
+          <Link to="/result"><button className='Check2' id='Checkid3' type='button'>자신의 성향 보기</button><button className='Check2' id='Checkid4'>></button></Link>
         </div>
       </div>
       <Menubar></Menubar>
