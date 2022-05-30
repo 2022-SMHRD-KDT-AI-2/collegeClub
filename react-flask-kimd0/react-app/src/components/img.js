a69dec0278394cac412e9dbef80e87d58a2019d8
강건우 <98583833+KangGunWoo@users.noreply.github.com>
2022-05-30 15:45:29 +0900

    123

---
+++ react-flask-kimd0/react-app/src/components/img.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './img.css';
+export default class FSlider extends Component {
+    render() {
+        const settings = {
+            dots: false,
+            autoplay: true,
+            infinite: true,
+            speed: 100,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
+        return (
+            <div>
+                <Slider {...settings}>
+                    <div className="sliimg">
+
+                    </div>
+                    <div className="sliimg2">
+
+                    </div>
+                    <div className="sliimg3">
+
+                    </div>
+                    <div className="sliimg4">
+
+                    </div>
+                </Slider>
+            </div>
+        );
+    }
+}   
